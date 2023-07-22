0668825e5aa45ddd975d45c6ead13eff4f83c560
Jeet_Patel <kasundrajeet529@gmail.com>
2023-07-22 12:48:30 +0530

    add script

--- js/main.js
+++ js/main.js
@@ -16,3 +16,76 @@ $("#MenuClose").click(function () {
     $("#Overlay").addClass("hidden");
 });
 
+$("#Overlay").click(function () {
+    $("#Menu").addClass("max-[698px]:hidden");
+    $(this).addClass("hidden");
+})
+
+$(".MenuLink").click(function () {
+    $("#Menu").addClass("max-[698px]:hidden");
+    $("#Overlay").addClass("hidden");
+})
+
+
+
+$(document).ready(function () {
+    var currentDate = new Date();
+
+    var monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    var dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    var month = monthNames[currentDate.getMonth()];
+    var dayOfWeek = dayNames[currentDate.getDay()];
+    var date = currentDate.getDate();
+    var year = currentDate.getFullYear();
+
+    var formattedDate = month + " " + date + ", " + year;
+
+
+    function updateTime() {
+        var currentTime = new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: true });
+        $("#currentTime").text(currentTime);
+    }
+
+    updateTime();
+
+    $("#currentDate").text(formattedDate);
+    $("#currentDay").text(dayOfWeek);
+    setInterval(updateTime, 1000);
+});
+
+
+function showModal() {
+    document.getElementById("myModal").style.display = "flex";
+}
+
+function hideModal() {
+    document.getElementById("myModal").style.display = "none";
+}
+
+
+function checkShopStatus() {
+    var currentTime = new Date();
+    var openingTime = new Date();
+    var closingTime = new Date();
+    var DayOfWeek = currentTime.getDay();
+
+
+
+    if (DayOfWeek === 0) {
+        // Sunday, set opening time to 10:00 AM and closing time to 12:30 PM
+        openingTime.setHours(10, 0, 0);
+        closingTime.setHours(12, 30, 0);
+    } else {
+        // Other days, set opening time to 10:00 AM and closing time to 08:00 PM
+        openingTime.setHours(10, 0, 0);
+        closingTime.setHours(20, 0, 0);
+    }
+
+    if (currentTime < openingTime || currentTime >= closingTime) {
+        showModal();
+    }
+}
+
+
+checkShopStatus();
